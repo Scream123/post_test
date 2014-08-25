08d591f1f077d61796b9b400a318efea4251bebd
samhavens <samhavens@gmail.com>
2014-08-25 16:16:57 -0700

    add timestamp

--- post_node.php
+++ post_node.php
@@ -4,8 +4,10 @@
 	$post_data .= "</pre>\n";
 
 	$filename = 'dump.txt';
+	$now      = date("Y-m-d H:i:s"); 
 
 	$handle = fopen($filename, 'a+');
+	fwrite($handle, $now);
 	fwrite($handle, $post_data);
 	fclose($handle);
 ?>
